39c255f60e8c244335a6a9413a5533a811c5e3a6
VasifMamedov <vasif.mamedov.1@iliauni.edu.ge>
2026-06-01 20:06:52 +0400

    preparing for neon postgreSQL server

--- src/config.py
+++ src/config.py
@@ -1,16 +1,25 @@
-from os import path
 import os
+from os import path
+
 
 class Config(object):
-
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    if os.environ.get('SECRET_KEY'):
-        SECRET_KEY = os.environ.get('SECRET_KEY')
-    else:
-        SECRET_KEY = 'secret'
-
     BASE_DIRECTORY = path.abspath(path.dirname(__file__))
+
+
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+
+    if DATABASE_URL:
+        if DATABASE_URL.startswith("postgres://"):
+            DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    else:
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{path.join(BASE_DIRECTORY, 'database.db')}"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
+
     UPLOAD_PATH = path.join(BASE_DIRECTORY, 'static', 'upload')
+
 
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_SUPPORTED_LOCALES = ['en', 'tr', 'ka']
